25fa19f16e836146aadf452b96b95746c3d6f6f6
Marine Crouzet <marine.m.crouzet@gmail.com>
2024-04-11 19:09:41 +0200

    set-up the app.py, to connect to db, retuning hello world in insomnia. Created the environment with local host with SQL. added pylint file for error handling. Installed all of the needed libraries

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask_bcrypt import Bcrypt
+
+from config.environment import db_URI 
+
+app = Flask(__name__)
+
+@app.route("/hello", methods=["GET"])
+def hello():
+    return "Hello, World!"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = db_URI
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+march = Marshmallow(app)
+bcrypt = Bcrypt(app) # Instantiate bcrypt
+
+# Import users AND teas controllers
+
+# Import Blueprints :
